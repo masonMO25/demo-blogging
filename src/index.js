54cc0f6c21a680adc034294183ee8d9e52a83b9b
masonMO25 <masonneale@gmail.com>
2022-10-25 20:44:42 -0500

    created new elements

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 const form = document.querySelector("form");
 const main = document.querySelector("main");
+
+const h1 = document.createElement("h1");
+const h2 = document.createElement("h2");
+const small = document.createElement("small");
+const p = document.createElement("p");
